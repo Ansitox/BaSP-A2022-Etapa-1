9096f82782404834c069aa04adfd228b4d612dbd
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-09-16 12:20:50 -0300

    BaSP-W05-2022: logs updated on 'variables and operators' and 'strings' exercises, arrays exercises done.

--- week-05/exercises/01_variables_and_operators.js
+++ week-05/exercises/01_variables_and_operators.js
@@ -1,7 +1,7 @@
 console.log('-----Exercise 01: VARIABLES AND OPERATORS.-----');
 
 /* a- Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la 
-suma de ambos números en una 3er variable. */
+    suma de ambos números en una 3er variable. */
 
 console.log('-----Excercise 01-a:-----');
 
@@ -9,10 +9,10 @@ var a = 13;
 var b = 5;
 var c = a + b;
 
-console.log(c);
+console.log('Addition result: ' + c);
 
 /* b- Crear dos variables de tipo String y concatenarlas 
-guardando el resultado en una 3er variable. */
+    guardando el resultado en una 3er variable. */
 
 console.log('-----Excercise 01-b:-----');
 
@@ -20,11 +20,11 @@ var part1 = 'This is ';
 var part2 = 'a string.';
 var text = part1 + part2;
 
-console.log(text);
+console.log('Concatenation: ' + text);
 
 /* c- Crear dos variables de tipo String y sumar el largo de cada variable 
-(cantidad de letras del string) guardando el resultado de la suma en una 3er variable 
-(utilizar length). */
+    (cantidad de letras del string) guardando el resultado de la suma en una 3er variable 
+    (utilizar length). */
 
 console.log('-----Excercise 01-c:-----');
 
@@ -32,4 +32,4 @@ var lengthVar1 = 'length';
 var lenghtVar2 = 'variable';
 var lengthAdditionVar = lengthVar1.length + lenghtVar2.length;
 
-console.log(lengthAdditionVar);
+console.log('Addition of the lengths: ' + lengthAdditionVar);
--- week-05/exercises/02_strings.js
+++ week-05/exercises/02_strings.js
@@ -8,7 +8,7 @@ console.log('-----Excercise 02-a:-----');
 var string1 = 'This is an excerise';
 string1 = string1.toUpperCase();
 
-console.log(string1);
+console.log('UpperCase string: ' + string1);
 
 /* b- Crear una variable de tipo string con al menos 10 caracteres y generar un 
     nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable 
@@ -17,9 +17,9 @@ console.log(string1);
 console.log('-----Excercise 02-b:-----');
 
 var string2 = 'Exercice 02, part b';
-var fistFive = string2.substring(0 , 5);
+var firstFive = string2.substring(0 , 5);
 
-console.log(fistFive);
+console.log('First five caracters: ' + firstFive);
 
 /* c- Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
     string con los últimos 3 caracteres guardando el resultado en una nueva variable 
@@ -30,7 +30,7 @@ console.log('-----Excercise 02-c:-----');
 var string3 = 'Exercise-02, part-c';
 var lastThree = string3.substring(string3.length - 3 , string3.length);
 
-console.log(lastThree);
+console.log('Last three characters: ' + lastThree);
 
 /* d- Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo 
     string con la primera letra en mayúscula y las demás en minúscula. Guardar el resultado en 
@@ -43,7 +43,7 @@ var capitalizedString =
     string4.substring(0 , 1).toUpperCase() + 
     string4.substring(1 , string4.length).toLowerCase();
 
-console.log(capitalizedString);
+console.log('Capitalized string: ' + capitalizedString);
 
 /* e- Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
     Encontrar la posición del primer espacio en blanco y guardarla en una variable 
@@ -52,9 +52,9 @@ console.log(capitalizedString);
 console.log('-----Excercise 02-e:-----');
 
 var string5 = 'Exercise 02, part e';
-var fistBlankSpacePosition = string5.indexOf(' ', 0);
+var firstBlankSpacePosition = string5.indexOf(' ', 0);
 
-console.log(fistBlankSpacePosition);
+console.log('Position of first blank space: ' + firstBlankSpacePosition);
 
 /* f- Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún 
     espacio entre medio). Utilizar los métodos de los ejercicios anteriores para generar un 
@@ -71,4 +71,4 @@ var capitalizedString2 =
     string6.substring(string6.indexOf(' ') + 1, string6.indexOf(' ') + 2).toUpperCase() +
     string6.substring(string6.indexOf(' ') + 2 , string6.length).toLocaleLowerCase();
 
-console.log(capitalizedString2);
+console.log('Capitalized strings: ' + capitalizedString2);
